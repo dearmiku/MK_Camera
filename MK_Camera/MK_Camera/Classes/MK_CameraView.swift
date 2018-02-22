ffb292a6d6f3892ede421913089c10fca7d166f1
杨尚达 <372154465@qq.com>
2018-02-22 15:44:42 +0800

    立项

---
+++ MK_Camera/MK_Camera/Classes/MK_CameraView.swift
@@ -0,0 +1,14 @@
+//
+//  MK_CameraView.swift
+//  MK_Camera
+//
+//  Created by MBP on 2018/2/22.
+//  Copyright © 2018年 MBP. All rights reserved.
+//
+
+import UIKit
+
+///相机视图
+class MK_Camera : UIView {
+
+}
