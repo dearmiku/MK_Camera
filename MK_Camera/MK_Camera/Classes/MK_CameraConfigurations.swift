6a7630bdf7ca4f899e840e4ea122e9d2f48f3f26
杨尚达 <372154465@qq.com>
2018-02-28 13:53:34 +0800

    Bug Fix

--- MK_Camera/MK_Camera/Classes/MK_CameraConfigurations.swift
+++ MK_Camera/MK_Camera/Classes/MK_CameraConfigurations.swift
@@ -13,71 +13,71 @@ import AVFoundation
 public struct MK_CameraConfigurations {
 
     ///闪光灯模式 默认自动模式
-    var flashMode = MK_RespValue<AVCaptureDevice.FlashMode>(AVCaptureDevice.FlashMode.auto)
+    public var flashMode = MK_RespValue<AVCaptureDevice.FlashMode>(AVCaptureDevice.FlashMode.auto)
 
     ///白平衡模式
-    var whiteBalanceMode = MK_RespValue<AVCaptureDevice.WhiteBalanceMode>(AVCaptureDevice.WhiteBalanceMode.autoWhiteBalance)
+    public var whiteBalanceMode = MK_RespValue<AVCaptureDevice.WhiteBalanceMode>(AVCaptureDevice.WhiteBalanceMode.autoWhiteBalance)
 
     ///聚焦模式
-    var fouceMode = MK_RespValue<AVCaptureDevice.FocusMode>(AVCaptureDevice.FocusMode.continuousAutoFocus)
+    public var fouceMode = MK_RespValue<AVCaptureDevice.FocusMode>(AVCaptureDevice.FocusMode.continuousAutoFocus)
     ///是否点击聚焦
-    var isTouchFouce = MK_RespValue<Bool>(true)
+    public var isTouchFouce = MK_RespValue<Bool>(true)
     ///手动调焦(默认关闭,当设置时将将关闭自动调焦,根据数值锁定焦距,值范围 0~1 从进到远)
-    var focalLength = MK_RespValue<Float?>(nil)
+    public var focalLength = MK_RespValue<Float?>(nil)
 
     ///曝光模式
-    var exportMode = MK_RespValue<AVCaptureDevice.ExposureMode>(AVCaptureDevice.ExposureMode.continuousAutoExposure)
+    public var exportMode = MK_RespValue<AVCaptureDevice.ExposureMode>(AVCaptureDevice.ExposureMode.continuousAutoExposure)
     ///手动设置曝光值(注意取值范围)
-    var exposureValue = MK_RespValue<Float?>(nil)
+    public var exposureValue = MK_RespValue<Float?>(nil)
 
     ///手动设置曝光时间(注意取值范围)
-    var exposureTime =  MK_RespValue<Double?>(nil)
+    public var exposureTime =  MK_RespValue<Double?>(nil)
     ///感光度(注意取值范围)
-    var iso = MK_RespValue<Float?>(nil)
+    public var iso = MK_RespValue<Float?>(nil)
     ///镜头方向
-    var position = MK_RespValue<AVCaptureDevice.Position>(AVCaptureDevice.Position.back)
+    public var position = MK_RespValue<AVCaptureDevice.Position>(AVCaptureDevice.Position.back)
 
     ///拍摄内容填充模式
-    var previewLayerMode = MK_RespValue<AVLayerVideoGravity>(AVLayerVideoGravity.resizeAspectFill)
+    public var previewLayerMode = MK_RespValue<AVLayerVideoGravity>(AVLayerVideoGravity.resizeAspectFill)
     
 }
 
 public extension MK_CameraView {
 
     ///最大曝光值
-    var maxExposureValue: Float{
+    public var maxExposureValue: Float{
         get{
             return device.maxExposureTargetBias
         }
     }
     ///最小曝光值
-    var minExposureValue:Float{
+    public var minExposureValue:Float{
         get{
             return device.minExposureTargetBias
         }
     }
 
     ///最大曝光时间
-    var maxExposureTime:Double{
+    public var maxExposureTime:Double{
         get{
             return self.device.activeFormat.maxExposureDuration.seconds / Double(self.device.activeFormat.maxExposureDuration.timescale)
         }
     }
     ///最小曝光时间
-    var minExposureTime:Double{
+    public var minExposureTime:Double{
         get{
             return self.device.activeFormat.minExposureDuration.seconds / Double(self.device.activeFormat.minExposureDuration.timescale)
         }
     }
 
     ///最大感光值
-    var maxISO:Float{
+    public var maxISO:Float{
         get{
             return self.device.activeFormat.maxISO
         }
     }
     ///最小感光值
-    var minISO:Float{
+    public var minISO:Float{
         get{
             return self.device.activeFormat.minISO
         }
